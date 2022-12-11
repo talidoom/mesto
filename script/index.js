f1d32da1e225b7af9931a623cf24e484606b98f9
Tori Elgina <talidoom@yandex.ru>
2022-12-11 20:20:52 +0300

    fix:edit style and script

--- script/index.js
+++ script/index.js
@@ -3,44 +3,40 @@ const popup = document.querySelector('.popup');
 const profileEditButton = content.querySelector('.profile__edit-button');
 const popupCloseButton = popup.querySelector('.popup__close-but');
 
-// функция для открытия попапа
-profileEditButton.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    popup.classList.add('popup_opened');
-});
-
-// функция для закрытия попапа
-popupCloseButton.addEventListener('click', function() {
-    popup.classList.remove('popup_opened');
-});
-popup.addEventListener('click', function(evt) {
-    if(evt.target === evt.currentTarget) {
-        popup.classList.remove('popup_opened');
-    }
-});
-
-
 const formElement = popup.querySelector('.form');
-const nameInput = formElement.querySelector('.form__text_name');
-const jobInput = formElement.querySelector('.form__text_job');
+const nameInput = formElement.querySelector('.form__text_type_name');
+const jobInput = formElement.querySelector('.form__text_type_job');
 const profileName = content.querySelector('.profile__name');
 const profileJob = content.querySelector('.profile__job');
 
-// автозаполнение формы из профиля
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
 
+// функция для открытия попапа
+function popupShow(evt) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+    popup.classList.add('popup_opened');
+}
+profileEditButton.addEventListener('click', popupShow);
 
-// функция обработчик «отправки» формы
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-
+// функция для закрытия попапа
+function popupClose () {
+    // автозаполнение формы из профиля
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+    // открытие окна
     popup.classList.remove('popup_opened');
 }
+popupCloseButton.addEventListener('click', popupClose);
 
+// функция отправки формы
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+  // заполнение профиля из формы
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+  // закрытие попапа
+    popup.classList.remove('popup_opened');
+}
 // срабатывание обработчика по кнопке
 formElement.addEventListener('submit', handleFormSubmit);
 
